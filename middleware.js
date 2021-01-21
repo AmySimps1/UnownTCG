e66e650368fad456f9b77b69d5084b86b422fe41
AmySimps1 <simpson.r.amy@gmail.com>
2021-01-21 17:46:34 +0000

    middleware added

---
+++ middleware.js
@@ -0,0 +1,52 @@
+const { productSchema, reviewSchema } = require('./schemas.js'),
+						 ExpressError = require('./utils/ExpressError'),
+						      Product = require('./models/product'),
+							   Review = require('./models/review');
+
+module.exports.isLoggedIn = (req, res, next) => {
+    if (!req.isAuthenticated()) {
+        req.session.returnTo = req.originalUrl
+        return res.redirect('/login');
+    }
+    next();
+}
+
+module.exports.validateProduct = (req, res, next) => {
+    const { error } = productSchema.validate(req.body);
+    console.log(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
+module.exports.isAuthor = async (req, res, next) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    if (!product.author.equals(req.user._id)) {
+        return res.redirect(`/products/${id}`);
+    }
+    next();
+}
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+    const { id, reviewId } = req.params;
+    const review = await Review.findById(reviewId);
+    if (!review.author.equals(req.user._id)) {
+        return res.redirect(`/products/${id}`);
+    }
+    next();
+}
+
+
+module.exports.validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
